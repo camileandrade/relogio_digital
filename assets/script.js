740b0552957195edefb13432ab979e0ede425937
camileandrade <camilefandrade@gmail.com>
2024-10-17 22:15:35 -0300

    primeiro commit

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+const elementoHora = document.getElementById('hora');
+const elementoMinutos = document.getElementById('minutos');
+const elementoSegundos = document.getElementById('segundos');
+
+const elementoDia = document.getElementById('dia');
+const elementoMes = document.getElementById('mes');
+const elementoAno = document.getElementById('ano');
+
+function formatar(v) {
+    return v < 10 ? '0' + v : v
+}
+
+function atualizarRelogio() {
+    const agora = new Date();
+
+    const dia = agora.getDate();
+    const mes = agora.getMonth();
+    const ano = agora.getFullYear();
+
+    elementoDia.textContent = formatar(dia);
+    elementoMes.textContent = formatar(mes + 1);
+    elementoAno.textContent = formatar(ano);
+
+    const hora = agora.getHours();
+    const minutos = agora.getMinutes();
+    const segundos = agora.getSeconds();
+
+    elementoHora.textContent = formatar(hora);
+    elementoMinutos.textContent = formatar(minutos);
+    elementoSegundos.textContent = formatar(segundos);
+
+    const dias = ['domingo', 'segunda', 'terca', 'quarta', 'quinta', 'sexta', 'sabado'];
+    const diaAtual = dias[agora.getDay()];
+
+    dias.forEach(dia => {
+        document.getElementById(dia).classList.remove('active');
+    });
+
+    document.getElementById(diaAtual).classList.add('active');
+
+}
+
+atualizarRelogio();
+setInterval(atualizarRelogio, 1000);
